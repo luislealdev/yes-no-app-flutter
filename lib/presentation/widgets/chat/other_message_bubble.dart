114d08b45eb18a43c797d7bb2ea5af8f3a00bea4
Luis Leal <luisrrleal@gmail.com>
2023-07-02 16:32:17 -0600

    Show loading image

--- lib/presentation/widgets/chat/other_message_bubble.dart
+++ lib/presentation/widgets/chat/other_message_bubble.dart
@@ -40,6 +40,14 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * .7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+              width: size.width * 70,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text("Olivia's sending a GIF..."));
+        },
       ),
     );
   }
