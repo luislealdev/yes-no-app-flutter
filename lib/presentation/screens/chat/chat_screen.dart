992cb07f42f0172d6d3a902fc0438b20fbd76ec7
Luis Leal <luisrrleal@gmail.com>
2023-07-02 17:38:19 -0600

    Finished ui

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app_flutter/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app_flutter/presentation/widgets/chat/other_message_bubble.dart';
+import 'package:yes_no_app_flutter/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -33,9 +34,12 @@ class _ChatView extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(children: [
         Expanded(child: ListView.builder(itemBuilder: (context, index) {
-          return (index % 2 == 0) ? OtherMessageBubble() : MyMessageBubble();
+          return (index % 2 == 0)
+              ? const OtherMessageBubble()
+              : const MyMessageBubble();
         })),
-        const Text('World')
+        //TEXTBOX
+        const MessageFieldBox()
       ]),
     ));
   }
---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(20));
+
+    final inputDecoration = InputDecoration(
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+            icon: const Icon(Icons.send_outlined),
+            onPressed: () {
+              final inputText = textController.value.text;
+              textController.clear();
+            }));
+
+    return TextFormField(
+      focusNode: focusNode,
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        textController.clear();
+      },
+    );
+  }
+}
